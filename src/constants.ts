36cf740322ee80c835c39f355087451acb987df9
isakshamkumar2 <saksham@genflowly.com>
2024-07-12 01:33:29 +0530

    [REFACTOR] Removed Github Actions and Moved HardCoded Values to Constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,6 @@
 export const LANGUAGES = ['EN', 'FR', 'ES'];
+export const NAV_ITEMS_LIST = ['Marketplace', 'Prompt Engineering', 'Prompt Chaining'];
+export const SEARCH_BAR_OPTIONS = ['Option 1', 'Option 2', 'Option 3'];
+export const COMPANY_NAME = 'Genflowly';
+export const SEARCHBARPLACEHOLDER = 'Search...';
+export const LOGINBUTTONTEXT = 'Login';
